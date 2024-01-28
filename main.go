ec32a77838b4409ce6fb03c1883ddae83f15acb5
Fraged <fragedd@gmail.com>
2024-01-28 21:00:30 +0300

    Загрузил с убунту

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var a, b, c, sum int
+	for fmt.Scanln(&a); a != 0; a = a / 10 {
+		b++
+	}
+
+	for b > 0 {
+		c = b % 10
+		b = b / 10
+		sum = sum + c
+	}
+	fmt.Println(sum)
 }
